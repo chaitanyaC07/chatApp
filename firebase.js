74a2e756f7007a54786ba888636bbc7f8aaa8027
chaitanyaC07 <95329729+chaitanyaC07@users.noreply.github.com>
2023-06-02 12:01:10 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBCYFtrTfZVVFac9xxjkJP3YAy6yOdsKds",
+  authDomain: "chatapps-7e4b3.firebaseapp.com",
+  projectId: "chatapps-7e4b3",
+  storageBucket: "chatapps-7e4b3.appspot.com",
+  messagingSenderId: "514212842205",
+  appId: "1:514212842205:web:938d1cfbb13ede6ab06934",
+};
+
+export const app = initializeApp(firebaseConfig);
